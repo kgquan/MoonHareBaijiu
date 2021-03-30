f9a5896fc5949b647d73e2acd0ad2b29ea7b431e
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2021-03-29 21:09:54 -0600

    Add header component CSS

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import homeStyles from '../styles/home.module.scss';
+import containerStyles from '../styles/components/container.module.scss';
+import headerStyles from '../styles/components/header.module.scss';
 
 export default function Home() {
   return (
     <>
+      <div className={headerStyles.header}>
+        <div className={containerStyles.container}>
+          <div className={headerStyles.header__logo}>
+            Logo
+          </div>
+          <div className={headerStyles.header__navigation}>
+            <ul>
+              <li>Shop</li>
+              <li>About</li>
+              <li>Recipes</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className={homeStyles.home}>Hello world!</div>
     </>
   );
