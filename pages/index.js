2ee5c3d924697caa4add10aad8fc2dc60babc5fc
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2021-03-31 01:06:26 -0600

    Add hero component CSS

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import shoppingCart2Line from '@iconify/icons-ri/shopping-cart-2-line';
 import homeStyles from '../styles/home.module.scss';
 import containerStyles from '../styles/components/container.module.scss';
 import headerStyles from '../styles/components/header.module.scss';
+import heroStyles from '../styles/components/hero.module.scss';
 
 export default function Home() {
   return (
@@ -25,7 +26,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={homeStyles.home}>Hello world!</div>
+      <div className={homeStyles.home}>
+        <div className={heroStyles.hero}>
+          <div className={containerStyles.container}>
+            <div className={heroStyles.hero__description}>
+              <h1>Make moments magical with our refined Chinese baijiu.</h1>
+              <p>
+                With a silky-smooth finish and a rich smokey flavour, our
+                traditionally-produced liquor is the perfect drink for any occasion.
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </>
   );
 }
