d05eb49429d11f21b22c4d0616f2e8bf506dd588
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2021-04-15 16:18:13 -0600

    Add list of actions for retrieving cart data

---
+++ context/actions.js
@@ -0,0 +1,4 @@
+/** The action when the user requests to retrieve cart data. */
+export const CART_RETRIEVE_REQUEST = 'CART_RETRIEVE_REQUEST';
+/** The action that confirms a cart has been successfully retrieved from the API. */
+export const CART_RETRIEVE_SUCCESS = 'CART_RETRIEVE_SUCCESS';
